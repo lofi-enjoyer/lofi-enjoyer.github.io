816f34a8762df59bf8ff74aeae59ef4ea612198d
lofi-enjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2025-05-10 12:52:41 +0100

    Add index page

--- src/app/index/index/index.component.ts
+++ src/app/index/index/index.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
