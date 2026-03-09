248e90224b91bcf945d335d04fc1a25dbcffb015
lofi-enjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2026-03-09 22:17:47 +0000

    Update 2026 songs

--- src/app/wv/wv-section/wv-section.component.ts
+++ src/app/wv/wv-section/wv-section.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-wv-section',
@@ -11,6 +12,18 @@ import {RouterLink, RouterOutlet} from '@angular/router';
   templateUrl: './wv-section.component.html',
   styleUrl: './wv-section.component.css'
 })
-export class WvSectionComponent {
+export class WvSectionComponent implements OnInit {
+
+  constructor(
+    private meta: Meta
+  ) {
+  }
+
+  ngOnInit() {
+    this.meta.updateTag({ property: 'og:title', content: 'WorldVisión' });
+    this.meta.updateTag({ property: 'og:description', content: 'Consulta aquí las participaciones de todas las ediciones de WorldVisión.' });
+    this.meta.updateTag({ property: 'og:url', content: 'https://lofi-enjoyer.github.io/wv' });
+    this.meta.updateTag({ property: 'og:image', content: 'https://lofi-enjoyer.github.io/wv2026-full.png' });
+  }
 
 }
